c9b6465f135ad0cc1f6caf5439e746dfc7e0125a
Tejas Bhatt <155186755+Tejas-PickupBiz@users.noreply.github.com>
2024-03-18 23:55:57 +0530

    sdasada

--- build/index.js
+++ build/index.js
@@ -3,11 +3,24 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = __importDefault(require("./app"));
 const database_1 = __importDefault(require("./DB/database"));
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const cors_1 = __importDefault(require("cors"));
+const routes_1 = __importDefault(require("./routes"));
+dotenv_1.default.config();
+const app = (0, express_1.default)();
+app.use((0, cors_1.default)());
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: true }));
+// routes 
+app.use("/api/v1/user", routes_1.default.authRouter);
 const PORT = process.env.PORT || 3000;
 (0, database_1.default)().then(() => {
-    app_1.default.listen(PORT, () => {
+    app.listen(PORT, () => {
         console.log(`Server running on port ${PORT}`);
     });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,33 @@
-import app from "./app";
 import connectDB from "./DB/database";
+
+import express, { Application ,Request,Response } from "express"
+import dotenv from "dotenv"
+import cors from "cors"
+import router from "./routes"
+
+dotenv.config()
+
+const app :Application = express()
+
+app.use(cors())
+
+app.get("/", (req:Request, res:Response) => {
+    res.send("Hello World");
+});
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+
+// routes 
+
+app.use("/api/v1/user",router.authRouter)
+
+
 const PORT = process.env.PORT || 3000;
 
 connectDB().then(
     () => {
-        app.listen(PORT, () => {
+        app.listen(PORT, ():void => {
             console.log(`Server running on port ${PORT}`);
         });
     }
